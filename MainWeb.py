d18771368088bcf43e2d7f252aef9e801a3bdd06
TAKASHI <hvasquezm3@upao.edu.pe>
2021-11-21 10:10:19 -0500

    ActualizacionFinal

--- MainWeb.py
+++ MainWeb.py
@@ -9,9 +9,9 @@ from PIL import Image
 st.set_page_config(page_title="HassPerú", page_icon="🥑")
 hide_st_style = """
             <style>
-            #MainMenu {visibility: hidden;}
+            --#MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
-            header {visibility: hidden;}
+            --header {visibility: hidden;}
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
